db4a67a6f3d85d7991ce15a7c7d0973b8da17fe8
jorge134 <jorgemartinezasuncion@gmail.com>
2014-12-01 19:42:49 +0100

    Confirmacion inicial

---
+++ Ahorcado 1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahorcado_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            Console.SetWindowSize(58, 58);
+        
+            Console.ForegroundColor = ConsoleColor.Green;
+
+             Console.WriteLine(" _____________  ______________________________________________ ");
+             Console.WriteLine("___    |__  / / /_  __ \\__  __ \\_  ____/__    |__  __ \\_  __ \\ ");
+             Console.WriteLine("__  /| |_  /_/ /_  / / /_  /_/ /  /    __  /| |_  / / /  / / / ");
+             Console.WriteLine("_  ___ |  __  / / /_/ /_  _, _// /___  _  ___ |  /_/ // /_/ /  ");
+             Console.WriteLine("/_/  |_/_/ /_/  \\____/ /_/ |_| \\____/  /_/  |_/_____/ \\____/   ");
+                                                              
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+          
+
+            Ahorcado a = new Ahorcado();
+            Console.WriteLine("Escribe la palabra a jugar:");
+            string palabra;
+
+            do{
+                palabra = Console.ReadLine();
+            }while(!a.setPalabra(palabra));
+            do{  //----------------------------------------------------Representar el penxat--------------------------------------------//
+                Console.Clear();
+                Console.WriteLine(a.mostrarPenxat());
+                Console.WriteLine("\r\n");
+                Console.WriteLine(a.PalabraOculta);
+                try
+                {
+                    a.setLletra(char.Parse(Console.ReadLine()));
+                }
+                catch (Exception ae){ }
+            } while (!a.comprobaStat());
+
+            Console.Clear();                     // Es netejara la consola cada vegada que se introduisca un caracter, per a que el ahorcado no se acumule.
+            Console.WriteLine(a.mostrarPenxat());
+            Console.WriteLine("\r\n");
+            Console.WriteLine(a.PalabraOculta);
+            Console.WriteLine("\r\n");
+            if (a.Vida == 0)
+            {
+                Console.WriteLine("Has perdut");
+            }
+            else
+            {
+                Console.Clear();
+
+                    Console.WriteLine(" ██╗  ██╗ █████╗ ███████╗     ██████╗  █████╗ ███╗   ██╗ █████╗ ██████╗  ██████╗     ██╗ ");
+                    Console.WriteLine(" ██║  ██║██╔══██╗██╔════╝    ██╔════╝ ██╔══██╗████╗  ██║██╔══██╗██╔══██╗██╔═══██╗    ██║ ");
+                    Console.WriteLine(" ███████║███████║███████╗    ██║  ███╗███████║██╔██╗ ██║███████║██║  ██║██║   ██║    ██║ ");
+                    Console.WriteLine(" ██╔══██║██╔══██║╚════██║    ██║   ██║██╔══██║██║╚██╗██║██╔══██║██║  ██║██║   ██║    ╚═╝ ");
+                    Console.WriteLine(" ██║  ██║██║  ██║███████║    ╚██████╔╝██║  ██║██║ ╚████║██║  ██║██████╔╝╚██████╔╝    ██╗ ");
+                    Console.WriteLine(" ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝  ╚═════╝     ╚═╝ ");
+                                   
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Ahorcado 1/game/Ahorcado.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ahorcado_1
+{
+    class Ahorcado
+    {
+        private string palabra;
+        private string palabraOculta;
+
+        public string PalabraOculta
+        {
+            get { return palabraOculta; }
+        }
+        private int vida;
+
+        public int Vida
+        {
+            get { return vida; }
+        }
+        public Ahorcado() //Números de intentos inicials.
+        {
+            vida = 6;
+        }
+        public string mostrarPenxat()    //-----------------------------------------Construcció del penxat----------------------------------------//
+        {
+            string penxat = "";
+            switch (this.vida)
+            {
+                case 0:
+                    penxat = "_________________ \r\n" +
+                            "  |            |  \r\n" +
+                            "  |            O  \r\n" +
+                            "  |           /|\\ \r\n" +
+                            "  |           / \\ \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            " /|\\              \r\n";
+                    break;
+                case 1:
+                    penxat = "_________________ \r\n" +
+                            "  |            |  \r\n" +
+                            "  |            O  \r\n" +
+                            "  |           /|\\ \r\n" +
+                            "  |           /   \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            " /|\\              \r\n";
+                    break;
+                case 2:
+                    penxat = "_________________ \r\n" +
+                            "  |            |  \r\n" +
+                            "  |            O  \r\n" +
+                            "  |           /|\\ \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            " /|\\              \r\n";
+                    break;
+                case 3:
+                    penxat = "_________________ \r\n" +
+                            "  |            |  \r\n" +
+                            "  |            O  \r\n" +
+                            "  |           /|  \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            " /|\\              \r\n";
+                    break;
+                case 4:
+                    penxat = "_________________ \r\n" +
+                            "  |            |  \r\n" +
+                            "  |            O  \r\n" +
+                            "  |            |  \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            " /|\\              \r\n";
+                    break;
+                case 5:
+                    penxat = "_________________ \r\n" +
+                            "  |            |  \r\n" +
+                            "  |            O  \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            " /|\\              \r\n";
+                    break;
+                case 6:
+                    penxat = "_________________ \r\n" +
+                            "  |            |  \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            "  |               \r\n" +
+                            " /|\\              \r\n";
+                    break;
+            }
+            return penxat;
+        }
+        public bool setPalabra(string palabra) //----------------------------Part de la representació de la paraula oculta------------------------//
+        {
+            if (!palabra.Equals("")) //Entrara si la cadena te algo dins
+            {
+                string PatronBusqueda = "0|1|2|3|4|5|6|7|8|9|_"; // Patró per a filtrar numeros en cas, que no se introduisquen lletres.
+                Regex regex = new Regex(PatronBusqueda, RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+                if (!regex.Match(palabra).Success)
+                {
+                    this.palabra = palabra; // ----------Estructura de representació de la paraula oculta.----------//
+                    this.palabraOculta = "";
+                    for (int i = 0; i < this.palabra.Length; i++)
+                    {
+                        this.palabraOculta += "_"; //Les lletres de la paraula introduida. es representara per _ per caracter.
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool comprobaStat(){ //----------------------------------- Part de finalizació del joc --------------------------------------------//
+            bool fin = true;
+            for (int i = 0; i < this.palabra.Length; i++)
+            {
+                if (palabraOculta[i].Equals('_'))//si la paraula conte alguna barra baixa, el joc no sacabat
+                {
+                    fin = false;//El joc no acaba
+                }
+            }
+            if (vida == 0)      //pero si les vides arriben a 0 dona = si te barres baixes o no
+            {
+                fin = true;     //El joc s'acaba
+            }
+            return fin;
+        }
+        public void setLletra(char lletra)  //-----------------------------------Part de la lletra presionada-------------------------------------// 
+        {
+            bool exist = false;
+            string cadena = "";
+            for (int i = 0; i < this.palabra.Length; i++)  //Recorrec la paraula.
+            {
+                if (this.palabra[i].Equals(lletra))    //Si la lletra coincideix en alguna posicio, safegeix a la paraula oculta.
+                {
+                    cadena += lletra;
+                    exist = true;
+                }
+                else    //sino, mante la lletra o _ que tenia
+                {
+                    cadena += this.palabraOculta[i];
+                }
+            }
+            if (!exist)   //si no existeix, reste una vida
+            {
+                this.vida--;
+            }
+            this.palabraOculta = cadena;
+        }
+    }
+}
